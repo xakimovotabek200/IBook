7d8a2ed542b06a15fe42d5af48baedc53c85e5fb
abduxoshimgafurov <abduxoshimgofurov79@gmail.com>
2023-10-24 21:50:40 +0500

    sasasaas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">salom1</h1>
+      <h1 className="text-red-500">salom2</h1>
     </div>
   );
 }
